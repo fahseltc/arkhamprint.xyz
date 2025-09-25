a96c6315a2fa0801f7a34d11c2689bb01ffa3385
Charles Fahselt <charles.fahselt@gmail.com>
2025-09-25 03:38:28 +0000

    more cleaning

--- db/migrate/20250924011356_create_pdf_documents.rb
+++
@@ -1,7 +0,0 @@
-class CreatePdfDocuments < ActiveRecord::Migration[8.0]
-  def change
-    create_table :pdf_documents do |t|
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20250924011421_add_file_to_documents.rb
+++
@@ -1,5 +0,0 @@
-class AddFileToDocuments < ActiveRecord::Migration[8.0]
-  def change
-    add_column :pdf_documents, :file, :string
-  end
-end
