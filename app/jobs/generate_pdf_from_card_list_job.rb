974b8ebc3ba84b26c6038bcce6ff80e0228d0217
Charles Fahselt <charles.fahselt@gmail.com>
2026-09-01 02:56:41 +0000

    add spacing and card options to all tabs.

--- app/jobs/generate_pdf_from_card_list_job.rb
+++ app/jobs/generate_pdf_from_card_list_job.rb
@@ -1,10 +1,17 @@
+require "uri"
+
 class GeneratePdfFromCardListJob < GeneratePdfBaseJob
+  DUPLEX_MODES = %w[none long_edge short_edge].freeze
+
   def perform(pdf_job_id, pdf_params)
     @pdf_job_id = pdf_job_id
     @pdf_job = PdfJob.find(@pdf_job_id)
     begin
-      @card_ids = Array(pdf_params["card_ids"]).map(&:to_s) if pdf_params["card_ids"].present?
+      @card_ids    = Array(pdf_params["card_ids"]).map(&:to_s) if pdf_params["card_ids"].present?
       raise ArgumentError, "card_ids must be present" unless @card_ids.present?
+      @print_backs = pdf_params["print_backs"] != false
+      @duplex_mode = DUPLEX_MODES.include?(pdf_params["duplex_mode"]) ? pdf_params["duplex_mode"] : "none"
+      @gap         = pdf_params["card_spacing"] == false ? PdfHelper::NO_GAP : PdfHelper::DEFAULT_GAP
       pdf_bin = generate_pdf_bin
       s3_key = upload_to_s3(pdf_bin)
     rescue PdfGenerationCancelled
@@ -15,16 +22,33 @@ class GeneratePdfFromCardListJob < GeneratePdfBaseJob
     end
   end
 
+  def generate_pdf_bin
+    cards_hash = get_cards_hash
+    images_per_card = @print_backs ? 2 : 1
+    @pdf_job.update!(max_progress: cards_hash.values.sum * images_per_card)
+
+    if @print_backs
+      records = cards_hash.map do |url, quantity|
+        base_id = File.basename(URI.parse(url).path, ".*")
+        back_url = ArkhamDbHelper.get_card_image_url("#{base_id}b")
+        { front_url: url, back_url: back_url, quantity: quantity }
+      end
+      PdfHelper.generate_with_backs(records, "LETTER", duplex_mode: @duplex_mode, gap: @gap, job_id: @pdf_job.id) do |idx|
+        report_progress(idx)
+      end
+    else
+      PdfHelper.generate(cards_hash, "LETTER", gap: @gap, job_id: @pdf_job.id) do |idx|
+        report_progress(idx)
+      end
+    end
+  end
+
   def get_cards_hash
     cards_hash = {}
     @card_ids.each do |id|
       img_url = ArkhamDbHelper.get_card_image_url(id)
       if img_url.present?
-        if cards_hash[img_url].nil?
-          cards_hash[img_url] = 1
-        else
-          cards_hash[img_url] += 1
-        end
+        cards_hash[img_url] = (cards_hash[img_url] || 0) + 1
       end
     end
     Rails.logger.info(cards_hash)
--- app/jobs/generate_pdf_from_deck_job.rb
+++ app/jobs/generate_pdf_from_deck_job.rb
@@ -1,4 +1,8 @@
+require "uri"
+
 class GeneratePdfFromDeckJob < GeneratePdfBaseJob
+  DUPLEX_MODES = %w[none long_edge short_edge].freeze
+
   def perform(pdf_job_id, pdf_params)
     @pdf_job_id = pdf_job_id
     @pdf_job = PdfJob.find(@pdf_job_id)
@@ -6,6 +10,9 @@ class GeneratePdfFromDeckJob < GeneratePdfBaseJob
       @deck_id = pdf_params["deck_id"]
       raise ArgumentError, "deck_id must be present" unless @deck_id.present?
       @include_investigator = pdf_params["include_investigator"]
+      @print_backs  = pdf_params["print_backs"] != false
+      @duplex_mode  = DUPLEX_MODES.include?(pdf_params["duplex_mode"]) ? pdf_params["duplex_mode"] : "none"
+      @gap          = pdf_params["card_spacing"] == false ? PdfHelper::NO_GAP : PdfHelper::DEFAULT_GAP
       pdf_bin = generate_pdf_bin
       s3_key = upload_to_s3(pdf_bin)
     rescue PdfGenerationCancelled
@@ -15,6 +22,28 @@ class GeneratePdfFromDeckJob < GeneratePdfBaseJob
       raise
     end
   end
+
+  def generate_pdf_bin
+    cards_hash = get_cards_hash
+    images_per_card = @print_backs ? 2 : 1
+    @pdf_job.update!(max_progress: cards_hash.values.sum * images_per_card)
+
+    if @print_backs
+      records = cards_hash.map do |url, quantity|
+        base_id = File.basename(URI.parse(url).path, ".*")
+        back_url = ArkhamDbHelper.get_card_image_url("#{base_id}b")
+        { front_url: url, back_url: back_url, quantity: quantity }
+      end
+      PdfHelper.generate_with_backs(records, "LETTER", duplex_mode: @duplex_mode, gap: @gap, job_id: @pdf_job.id) do |idx|
+        report_progress(idx)
+      end
+    else
+      PdfHelper.generate(cards_hash, "LETTER", gap: @gap, job_id: @pdf_job.id) do |idx|
+        report_progress(idx)
+      end
+    end
+  end
+
   def get_cards_hash
     cards = ArkhamDbHelper.get_cards_from_deck_id(@deck_id, @include_investigator).transform_keys { |card_id| ArkhamDbHelper.get_card_image_url(card_id) }
     Rails.logger.info(cards)
